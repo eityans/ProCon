bfa5ab1b5471090d2ec1d5dfc7a550cf37fb2fc1
eityans <eityans@gmail.com>
2019-08-04 23:20:47 +0900

    テンプレート

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,152 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.Set;
  
  
 public class Main {
+
+	static long MOD = 1_000_000_007;
+	static int INF = 1_000_000_007;
+	//“ñ€ŒW”ŠÖ˜A‚±‚±‚©‚ç-----
+	static boolean isPreprocessing = false;	//“ñ€ŒW”‚ğŒvZ‚·‚é‚½‚ß‚Ì‘Oˆ—‚ğs‚Á‚Ä‚¢‚é‚©‚Ç‚¤‚©‚Ìƒtƒ‰ƒO
+	static int MAX = 260;				//MAX‚Ü‚ÅŠKæ‚â‚»‚ê‚ç‚Ì‹tŒ³‚Ìƒe[ƒuƒ‹‚ğ•Û‚·‚éB
+	static long[] fac;	//fac[i] := i! % MOD
+	static long[] inv;	//inv[i] := mod. MOD‚É‚¨‚¯‚é i ‚Ì‹tŒ³
+	static long[] finv;	//finv[i] := mod. MOD‚É‚¨‚¯‚é i! ‚Ì‹tŒ³(inv‚Ì—İÏ"Ï")
+	//“ñ€ŒW”ŠÖ˜A‚¨‚í‚è------
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+	static int[] roots;
+	
+	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
-		int T = Integer.parseInt(sc.next());
-		for(int tr=0; tr<T; tr++){
-			char[] X = sc.next().toCharArray();
-			char[] Y = sc.next().toCharArray();
-			int X_len = X.length;
-			int Y_len = Y.length;
-			int[][] dp = new int[X_len+1][Y_len+1];
+		//int N = Integer.parseInt(sc.next());
+		//long L = Long.parseLong(sc.next());
+		//char[] A = sc.next().toCharArray();
 			
-			for(int x=0; x<X_len; x++){
-				for(int y=0; y<Y_len; y++){
-					if(X[x] == Y[y]){
-						dp[x+1][y+1] = dp[x][y]+1;
-					}else{
-						dp[x+1][y+1] = Math.max(dp[x+1][y],dp[x][y+1]);
+		
+		
+	}
+	
+	static boolean same(int a, int b){
+		if(root(a) == root(b)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	static int root(int a){
+		if(a == roots[a])return a;
+		return roots[a] = root(roots[a]);
+	}
+	
+	static void join(int a, int b){
+		if(root(a) == root(b))return;
+		roots[b] = root(a);
+	}
+	
+	//‚Q‚Â‚Ìint”z—ñ‚ğ˜AŒ‹‚³‚¹‚é
+	static int[] joinArray(int[] A, int[] B){
+		int[] tmp = new int[A.length + B.length];
+		System.arraycopy(A, 0, tmp, 0, A.length);
+		System.arraycopy(B, 0, tmp, A.length, B.length);
+		return tmp;
+	}
+	
+	//‚Q‚Â‚Ìint”z—ñ‚ª“™‚µ‚¢‚©‚Ç‚¤‚©”»’è‚·‚é
+	static boolean arrayEqual(int[] A, int[] B){
+		if(A.length != B.length)return false;
+		for(int i=0; i<A.length; i++){
+			if(A[i] != B[i])return false;
+		}
+		return true;
+	}
+	
+	//int”z—ñ‚ğindex‡‚ÉŒ©‚ÄA—v‘f‚Ìd•¡‚ª‚ ‚Á‚½ê‡‚»‚Ì—v‘f‚ÆŠÔ‚Ì—v‘f‚ğíœ‚·‚é12123¨23
+	static int[] calc(int[] A){
+		boolean[] tmp = new boolean[A.length];
+		Arrays.fill(tmp, true);
+		int fetch = 0;
+		while(fetch<A.length-1){
+			boolean isExist = false;
+			for(int i=fetch+1; i<A.length;i++){
+				if(A[fetch] == A[i]){
+					for(int j=fetch;j<=i;j++){
+						tmp[j]=false;
 					}
+					fetch = i+1;
+					isExist = true;
+					break;
 				}
 			}
-			System.out.println(dp[X_len][Y_len]);
+			if(!isExist)fetch++;
 		}
-		sc.close();
+		List<Integer> list = new ArrayList<>();
+		for(int i=0; i<A.length; i++){
+			if(tmp[i])list.add(A[i]);
+		}
+		
+		int[] foo = new int[list.size()];
+		for(int i=0; i<list.size(); i++){
+			foo[i] = list.get(i);
+		}
+		/*
+		for(boolean t : tmp)
+			System.out.print(t+" ");
+		*/
+		return foo;
 	}
 	
 	
+	//a‚Æb‚ÌÅ¬Œö”{”‚ğ•Ô‚·
+	static long lcm (long a, long b) {
+		long temp;
+		long c = a;
+		c *= b;
+		while((temp = a%b)!=0) {
+			a = b;
+			b = temp;
+		}
+		return (c/b);
+	}
+	
+	//“ñ€ŒW”—p‚Ì‘Oˆ—
+	static void COMinit(){
+		//“ñ€ŒW”‰‰Z—p‘Oˆ—
+		fac = new long[MAX+1];
+		fac[0] = 1L;
+		fac[1] = 1L;
+		inv = new long[MAX+1];
+		inv[1] = 1L;
+		finv = new long[MAX+1];
+		finv[0] = 1L;
+		finv[1] = 1L;
+		for(int i=2;i<MAX;i++){
+			fac[i] = fac[i-1] * i % MOD;
+			inv[i] = MOD - inv[(int)(MOD%i)] * (MOD/i) % MOD;
+			finv[i] = finv[i-1] * inv[i] % MOD;
+		}
+		isPreprocessing = true;
+	}
+	
+	//“ñ€ŒW”i_C_j‚ğMOD‚ÅŠ„‚Á‚½—]‚è‚ğ•Ô‚·
+	static long COM(int i, int j){
+		if(i<j)return 0;
+		if(i<0 || j<0)return 0;
+		if(!isPreprocessing)COMinit();
+		return fac[i] * (finv[j] * finv[i-j] % MOD) % MOD;
+	}
 	
 }
+
